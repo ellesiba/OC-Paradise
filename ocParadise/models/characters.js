db1e6215802643a4b2177f8fc3c806d5b20a79a2
ellesiba <rochelle.sibaja@gamil.com>
2023-03-11 02:36:52 -0500

    Added temp filler for character and added server.js

---
+++ ocParadise/models/characters.js
@@ -0,0 +1,16 @@
+const character = [
+    {
+        name: 'char 1',
+    },
+    {
+        name: 'chara 2',
+    },
+    {
+        name: 'chara 3'
+    },
+    {
+        name: 'chara 4',
+    }
+];
+
+module.exports = character;
