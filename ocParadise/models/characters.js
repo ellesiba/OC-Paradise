978f94533bb5c807fce80b8b35a7669a443665cc
ellesiba <rochelle.sibaja@gamil.com>
2023-03-19 03:30:59 -0400

    Skeleton and seeding

--- ocParadise/models/characters.js
+++ ocParadise/models/characters.js
@@ -1,16 +1,29 @@
-const character = [
-    {
-        name: 'char 1',
-    },
-    {
-        name: 'chara 2',
-    },
-    {
-        name: 'chara 3'
-    },
-    {
-        name: 'chara 4',
-    }
-];
+const mongoose = require('mongoose');
 
-module.exports = character;
+const charaSchema = new mongoose.Schema({
+    image: { type: String },
+    name: { type: String, required: true },
+    birthday: { type: String, required: true },
+    zodiac: {type: String, enum: ['Aries', 'Taurus', 'Gemini','Cancer','Leo','Virgo','Libra','Scorpio','Sagittarius','Capricorn','Aquarius','Pisces']},
+    birthplace: { type: String, required: true },
+    bio: {type: String, required: true},
+    idiosyncrasy: { type: String, required: true},
+    personality:{type: String, required: true},
+    relationships: {type: String},
+    goalsMotivations:{type: String},
+    fearsWeakness: {type: String},
+    skills:{type: String},
+    valueBeliefs:{type: String},
+    fearsWeakness:{type: String},
+    mostTreasured:{type: String},
+    askForPermissionToUse: { type: Boolean, required: true },
+    originalCreator: { type: String, required: true },
+    socialMedia: {
+        instagram: { type: String },
+        twitter: { type: String },
+        tiktok: { type: String }
+      }
+});
+
+const chara = mongoose.model('chara', charaSchema);
+module.exports = chara;
